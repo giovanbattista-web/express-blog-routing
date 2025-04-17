9df93a57098c50c3f793ba383eae80e11e58e6ce
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 17:46:04 +0200

    [UPD] SPAZIO CREATO NELLA RISPOSTA

--- routers/posts.js
+++ routers/posts.js
@@ -6,19 +6,19 @@ router.get('/',(req,res) =>{
     res.send('Lista dei post');
 })
 router.get('/:id',(req,res) =>{
-    res.send("Dettaglio del post" + req.params.id);
+    res.send(`Dettaglio del post ${req.params.id}`);
 })
 router.post('/',(req,res) =>{
-    res.send('Inserimento di un nuovo post');
+    res.send("Inserimento di un nuovo post");
 })
 router.put('/:id',(req,res) =>{
-    res.send("Modifica totale del post" + req.params.id);
+    res.send(`Modifica totale del post ${req.params.id}`);
 })
 router.patch('/:id',(req,res) =>{
-    res.send("Modifica parziale del post" + req.params.id);
+    res.send(`Modifica parziale del post ${req.params.id}`);
 })
 router.delete('/:id',(req,res) =>{
-    res.send("Cancellazione del post" + req.params.id);
+    res.send(`Cancellazione del post ${req.params.id}`);
 })
 
 module.exports = router;
