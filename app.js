20eb705a585e479bd59be1f7e79f2a1080697067
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 16:48:30 +0200

    [UPD] ROTTE RELATIVE AI POST

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
     res.send("Home Page");
 })
 
+app.get('/posts', (req, res) => {
+    res.send("Lista dei post");
+})
+app.get('/posts/:id', (req, res) => {
+    res.send("Dettagli del post" + req.params.id);
+})
+app.post('/posts', (req, res) => {
+    res.send("Inserimento di un nuovo post");
+})
+app.put('/posts/:id', (req, res) => {
+    res.send("Modifica totale del post" + req.params.id);
+})
+app.patch('/posts/:id', (req, res) => {
+    res.send("Modifica parziale del post" + req.params.id);
+})
+app.delete('/posts/:id', (req, res) => {
+    res.send("Rimozione del post" + req.params.id);
+})
+
 app.listen(port, () => {
     console.log(`Server in ascolto alla porta ${port}`);
 })
--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
+
+router.get('/',(req,res) =>{
+    res.send('Lista dei post');
+})
+router.get('/:id',(req,res) =>{
+    res.send("Dettagli del post" + req.params.id);
+})
+router.post('/',(req,res) =>{
+    res.send('Inserimento di un nuovo post');
+})
+router.put('/:id',(req,res) =>{
+    res.send("Modifica totale del post" + req.params.id);
+})
+router.patch('/:id',(req,res) =>{
+    res.send("Modifica parziale del post" + req.params.id);
+})
+router.delete('/:id',(req,res) =>{
+    res.send("Rimozione del post" + req.params.id);
+})
+
