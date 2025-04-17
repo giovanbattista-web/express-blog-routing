8406183d3b9e523573746f826fb4fa3c7810bcdd
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 15:54:19 +0200

    [UPD] IMPORTAZIONE DI EXPRESS E DELLA CLASSE ROUTER

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send("Home Page");
 })
 
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log(`Server in ascolto alla porta ${port}`);
 })
