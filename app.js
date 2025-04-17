aafb320f30179466ee72fbe0aec8f93d49cdb017
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-17 14:53:37 +0200

    [UPD] ROTTA BASE  E SERVER IN ASCOLTO

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/',(req,res) =>{
+    res.send('post');
+})
+
+app.listen(port,() =>{
+    console.log(`Server in ascolto alla porta ${port}`);
+})
